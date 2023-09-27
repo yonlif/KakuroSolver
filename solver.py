8378c7b33ec6854215e478659d283ef9b9e4f692
Yonatan Lifshitz <yonlif@gmail.com>
2023-09-27 15:39:20 -0700

    reformat and optimizations

--- solver.py
+++ solver.py
@@ -1,3 +1,5 @@
+from typing import Tuple
+
 from board import KakoroBoard, Cell, BlackCell, ConstraintCell
 from combination_calculator import combinations_with_option_numbers_to_sum
 
@@ -7,8 +9,8 @@ def reduce_all_options(board: KakoroBoard) -> None:
     If a number was added to the board - use this function to eliminate the options to use this
     number from other cells
     """
-    for y, row in enumerate(board.board):
-        for x, item in enumerate(row):
+    for row in board.board:
+        for item in row:
             if type(item) == Cell:
                 if item.value:
                     for connected_cell in item.visible_cells:
@@ -16,7 +18,8 @@ def reduce_all_options(board: KakoroBoard) -> None:
                             connected_cell.option_numbers.remove(item.value)
 
 
-def _brute_force_constraint_cell(constraint_cell: ConstraintCell) -> None:
+def _brute_force_constraint_cell(constraint_cell: ConstraintCell) -> bool:
+    progress_made = False
     for constraint_cell in [constraint_cell.right, constraint_cell.down]:
         if not constraint_cell.visible_cells or constraint_cell.is_finished:
             continue
@@ -30,134 +33,36 @@ def _brute_force_constraint_cell(constraint_cell: ConstraintCell) -> None:
         elif len(results) == 1:
             constraint_cell.is_finished = True
             for set_value, set_cell in zip(results[0], constraint_cell.visible_cells):
-                set_cell.value = set_value
+                if set_cell.value != set_value:
+                    progress_made = True
+                    set_cell.value = set_value
         else:
             for idx, set_cell in enumerate(constraint_cell.visible_cells):
+                if set_cell.value is not None:
+                    continue
                 option_numbers_set = set()
                 for res in results:
                     option_numbers_set.add(res[idx])
-                set_cell.option_numbers = list(set(set_cell.option_numbers) & option_numbers_set)
+                new_option_numbers = sorted(list(set(set_cell.option_numbers) & option_numbers_set))
+                if new_option_numbers != set_cell.option_numbers:
+                    progress_made = True
+                    set_cell.option_numbers = new_option_numbers
+    return progress_made
 
 
-def brute_force_iteration(board):
+def brute_force_iteration(board) -> bool:
+    progress_made = False
     for y, row in enumerate(board.board):
         for x, item in enumerate(row):
             if type(item) == ConstraintCell:
-                _brute_force_constraint_cell(item)
+                progress_made |= _brute_force_constraint_cell(item)
+    return progress_made
 
 
-def test():
-    b = KakoroBoard([[BlackCell(), BlackCell(), ConstraintCell(down_result=3), BlackCell()],
-                     [ConstraintCell(right_result=3), Cell(idx=1), Cell(idx=2), BlackCell()],
-                     [BlackCell(), ConstraintCell(right_result=5), Cell(idx=3), Cell(3, idx=4)],
-                     [BlackCell(), BlackCell(), BlackCell(), BlackCell()],
-                     ])
-    reduce_all_options(b)
-    print(b)
-    brute_force_iteration(b)
-    print(b)
-    brute_force_iteration(b)
-    print(b)
-    brute_force_iteration(b)
-    print(b)
-
-
-def test2():
-    b = KakoroBoard([[BlackCell(), ConstraintCell(down_result=11), ConstraintCell(down_result=3)],
-                     [ConstraintCell(right_result=3), Cell(), Cell()],
-                     [ConstraintCell(right_result=11), Cell(), Cell()]])
-    reduce_all_options(b)
-    print(b)
-    brute_force_iteration(b)
-    print(b)
-
-
-def test3():
-    b = KakoroBoard([[BlackCell(), ConstraintCell(down_result=14), ConstraintCell(down_result=7)],
-                     [ConstraintCell(right_result=15), Cell(), Cell()],
-                     [ConstraintCell(right_result=6), Cell(), Cell()]])
-    reduce_all_options(b)
-    print(b)
-    brute_force_iteration(b)
-    print(b)
-
-
-def test4():
-    b = KakoroBoard(
-        [[BlackCell(), BlackCell(), ConstraintCell(down_result=10), ConstraintCell(down_result=29), BlackCell()],
-         [BlackCell(), ConstraintCell(right_result=9, down_result=16), Cell(), Cell(), ConstraintCell(down_result=4)],
-         [ConstraintCell(right_result=15), Cell(), Cell(), Cell(), Cell()],
-         [ConstraintCell(right_result=23), Cell(), Cell(), Cell(), Cell()],
-         [BlackCell(), ConstraintCell(right_result=12), Cell(), Cell(), BlackCell()]])
-    reduce_all_options(b)
-    print(b)
-    brute_force_iteration(b)
-    print(b)
-    brute_force_iteration(b)
-    print(b)
-    brute_force_iteration(b)
-    print(b)
-
-
-def test5():
-    """
-    #  14\  5\  #   #
-   \13  9   4  11\  #
-   \12  0   1  0    #
-    #   #  \6  0    0
-    """
-    b = KakoroBoard(
-        [[BlackCell(), ConstraintCell(down_result=14), ConstraintCell(down_result=5), BlackCell(), BlackCell()],
-         [ConstraintCell(right_result=13), Cell(9), Cell(4), ConstraintCell(down_result=10), BlackCell()],
-         [ConstraintCell(right_result=12), Cell(), Cell(1), Cell(), BlackCell()],
-         [BlackCell(), BlackCell(), ConstraintCell(right_result=10), Cell(), Cell()]])
-    reduce_all_options(b)
-    print(b)
-    brute_force_iteration(b)
-    print(b)
-    brute_force_iteration(b)
-    print(b)
-    brute_force_iteration(b)
-    print(b)
-
-
-def test6():
-    """
-      #   3\   16\  29\  #   #   #   #   #
-     \15  0    9    0   11\  #   #   #   #
-     \26  0    7    0    0   9   4   #   #
-     #    #    \7   0    0   #   #   #   #
-     #    #   \14   8    5   1   #   #   #
-     #    #  28\28  0    0   2   5   8   9
-     #   \11   0    0    #   #   #   #   #
-     #    \9   0    0    #   #   #   #   #
-     #    #    5    #    #   #   #   #   #
-     #    #    4    #    #   #   #   #   #
-     #    #    1    #    #   #   #   #   #
-     #    #    3    #    #   #   #   #   #
-     #    #    2    #    #   #   #   #   #
-    """
-    b = KakoroBoard(
-        [[BlackCell(), ConstraintCell(down_result=3), ConstraintCell(down_result=16),
-          ConstraintCell(down_result=29)] + [BlackCell()] * 5,
-         [ConstraintCell(right_result=15), Cell(), Cell(9), Cell(), ConstraintCell(down_result=11)] + [BlackCell()] * 4,
-         [ConstraintCell(right_result=26), Cell(), Cell(7), Cell(), Cell(), Cell(9), Cell(4)] + [BlackCell()] * 2,
-         [BlackCell()] * 2 + [ConstraintCell(right_result=7), Cell(), Cell()] + [BlackCell()] * 4,
-         [BlackCell()] * 2 + [ConstraintCell(right_result=14), Cell(8), Cell(5), Cell(1)] + [BlackCell()] * 3,
-         [BlackCell()] * 2 + [ConstraintCell(down_result=28, right_result=28), Cell(), Cell(), Cell(2), Cell(5), Cell(8), Cell(9)],
-         [BlackCell(), ConstraintCell(right_result=11), Cell(), Cell()] + [BlackCell()] * 5,
-         [BlackCell(), ConstraintCell(right_result=9), Cell(), Cell()] + [BlackCell()] * 5,
-         [BlackCell()] * 2 + [Cell(1)] + [BlackCell()] * 6,
-         [BlackCell()] * 2 + [Cell(2)] + [BlackCell()] * 6,
-         [BlackCell()] * 2 + [Cell(3)] + [BlackCell()] * 6,
-         [BlackCell()] * 2 + [Cell(4)] + [BlackCell()] * 6,
-         [BlackCell()] * 2 + [Cell(5)] + [BlackCell()] * 6,
-         ])
-    reduce_all_options(b)
-    for _ in range(8):
-        brute_force_iteration(b)
-        print(b)
-
-
-if __name__ == '__main__':
-    test6()
+def solve_loop(board: KakoroBoard) -> Tuple[int, bool]:
+    counter = 0
+    progress = True
+    while progress:
+        counter += 1
+        progress = brute_force_iteration(board)
+    return counter, board.is_solved()
